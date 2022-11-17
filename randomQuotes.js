f0146ce91aa22af62e11759db204d7d3bcbfb991
William Martinez <wrmartinez709@gmail.com>
2022-11-17 14:39:41 -0500

    Change function name and fix conditional statement

--- randomQuotes.js
+++ randomQuotes.js
@@ -1,21 +1,16 @@
-function randomMessage(num) {
-    num = Math.floor(Math.random() * 5);
-    if (num === 0) {
+const randomQuote = () => {
+    const quoteSelector = Math.floor(Math.random() * 5);
+    if (quoteSelector === 0) {
         return "“We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein";
-    }
-    if (num === 1) {
+    } else if (quoteSelector === 1) {
         return "“Learn as if you will live forever, live like you will die tomorrow.” — Mahatma Gandhi";
-    }
-    if (num === 2) {
+    } else if (quoteSelector === 2) {
         return "“Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.” — Mark Twain";
-    }
-    if (num === 3) {
+    } else if (quoteSelector === 3) {
         return "“When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.”— Eleanor Roosevelt";
-    }
-    if (num === 4) {
+    } else if (quoteSelector === 4) {
         return "“When you change your thoughts, remember to also change your world.”— Norman Vincent Peale";
-    }
-    if (num === 5) {
-        return "“It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest. —Walter Anderson";
+    } else if (quoteSelector === 5) {
+        return "“It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest. — Walter Anderson";
     }
 }
